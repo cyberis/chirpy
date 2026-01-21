ddf4cf968a6313d0084a6136528871f837e42f5a
Cyberis <christopher.bortz@gmail.com>
2026-01-21 01:39:58 -0500

    Create Minimal Web Server

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	serverMux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: serverMux,
+	}
+
+	server.ListenAndServe()
+}
